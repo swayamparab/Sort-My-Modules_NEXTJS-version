b92f86299ce8c0708fceba039e12f5fe927622aa
swayam <swayam.parab2203@gmail.com>
2026-06-02 15:41:38 +0530

    fix toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 
 import { AuthProvider } from "@/context/AuthContext";
 import { ThemeProvider } from "@/context/ThemeContext";
+import { Toaster } from "react-hot-toast";
 
 export default function RootLayout({
   children,
@@ -14,6 +15,16 @@ export default function RootLayout({
       <body>
         <AuthProvider>
           <ThemeProvider>
+            <Toaster
+              position="top-center"
+              toastOptions={{
+                style: {
+                  background: "#1E2A3A",
+                  color: "#fff",
+                  borderRadius: "8px"
+                }
+              }}
+            />
             <Navbar />
             <main className="container">
               {children}
